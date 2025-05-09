9a930bd0352969790d56b52a1671a786e410d1a6
Edgardo Silva <e.silva.plaza@gmail.com>
2025-05-09 16:42:12 +0200

    new format

--- main.py
+++ main.py
@@ -7,20 +7,21 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from numpy.ma.core import zeros
 
 """
 List of variables
-age, page = Age - anchor/partner 
-sex, psex = Sex - anchor/partner
+age =  Age - anchor/partner 
+sex = Sex - anchor/partner
 reldur = Relationship duration in years
 reldur_class = Relationship duration [years]
 cohab = Cohabitation
 nkidsliv_class = Number of all children living with Anchor 
-relsat, prelsat = Relationship satisfaction - anchor/partner
-relin prelint =  Relationship quality - anchor/partner
-tf_kiss, ptf_kiss = Touch frequency: Kissing - anchor/partner 
-tf_hold, ptf_hold = Touch frequency: Holding - anchor/partner 
-tf_hug, ptf_hug = Touch frequency: Hugging - anchor/partner 
+relsat = Relationship satisfaction - anchor/partner
+relin =  Relationship quality - anchor/partner
+tf_kiss = Touch frequency: Kissing - anchor/partner 
+tf_hold = Touch frequency: Holding - anchor/partner 
+tf_hug = Touch frequency: Hugging - anchor/partner 
 
 omo15i1_w2b, pomo15i1_w2b = Touch frequency kiss last week - anchor/partner 
 omo16i1_w2b, pomo16i1_w2b = Touch frequency put arm around shoulder last week - anchor/partner 
@@ -31,39 +32,86 @@ omo16i2_w2b, pomo16i2_w2b = Desire to touch put arm around shoulder last week - 
 omo17i2_w2b, pomo17i2_w2b = Desire to touch intimate hug last week - anchor/partner 
 """
 
-df = pd.read_csv("source/FReDA.csv", delimiter=";")
+df = pd.read_csv("source/only_couples_data.csv", delimiter=",")
+index_options = {"0x": 0,
+                 "1-5x": 1,
+                 "6-10x": 2,
+                 "11-20x": 3,
+                 "21-50x": 4,
+                 "> 50x": 5}
 
-sns.countplot(data=df, x="omo15i1_w2b", hue="type",
-              order=["Not at all", "1 to 5 times", "6 to 10 times", "11 to 20 times", "21 to 50 times",
-                     "More than 50 times"])
-plt.title("OMO15 - Touch frequency kiss last week / Received")
-plt.xlabel("Frequency")
-plt.ylabel("Count")
+df["kiss_diff"] = None
+for i in range(0, len(df)):
+    print(i, index_options[df['ptf_kiss'][i]] - index_options[df['tf_kiss'][i]])
+    if index_options[df['tf_kiss'][i]] == index_options[df['ptf_kiss'][i]]:
+        df.loc[i, "kiss_diff"] = 0
+    elif index_options[df['tf_kiss'][i]] > index_options[df['ptf_kiss'][i]] < 2:
+        df.loc[i, "kiss_diff"] = 1
+    elif (index_options[df['tf_kiss'][i]] - index_options[df['ptf_kiss'][i]]) >= 2:
+        df.loc[i, "kiss_diff"] = 2
+    elif index_options[df['tf_kiss'][i]] < index_options[df['ptf_kiss'][i]] < 2:
+        df.loc[i, "kiss_diff"] = -1
+    elif (index_options[df['ptf_kiss'][i]] - index_options[df['tf_kiss'][i]]) >= 2:
+        df.loc[i, "kiss_diff"] = -2
+print(df)
 
-# sns.countplot(data=df, x="omo15i2_w2b", hue="type",
+#     if df['tf_hold'][i] == df['ptf_hold'][i]:
+#         kiss_diff[i] = 0
+# value = label_to_value[selected_option]
+#
+# print(df["match"])
+
+# fig1, axes = plt.subplots(2, 1)
+# fig1.suptitle("OMO15 - Touch frequency kiss last week")
+# sns.countplot(ax=axes[0], data=df, x="omo15i1_w2b", hue="type",
 #               order=["Not at all", "1 to 5 times", "6 to 10 times", "11 to 20 times", "21 to 50 times",
 #                      "More than 50 times"])
-# plt.title("OMO15 - Touch frequency kiss last week / Desired")
-# plt.xlabel("Frequency")
-# plt.ylabel("Count")
-
-plt.show()
-
-# TODO: GIT UNTRACK
-
-"""
-fig, axes = plt.subplots(1, 3, figsize=(15, 5), sharey=True)
-fig.suptitle('Initial Pokemon - 1st Generation')
-
-# Bulbasaur
-sns.barplot(ax=axes[0], x=bulbasaur.index, y=bulbasaur.values)
-axes[0].set_title(bulbasaur.name)
-
-# Charmander
-sns.barplot(ax=axes[1], x=charmander.index, y=charmander.values)
-axes[1].set_title(charmander.name)
-
-# Squirtle
-sns.barplot(ax=axes[2], x=squirtle.index, y=squirtle.values)
-axes[2].set_title(squirtle.name)
-"""
+# axes[0].set_title("Received")
+# axes[0].set_xlabel("Frequency")
+# axes[0].set_ylabel("Count")
+# axes[0].set_ylim([0, 2500])
+#
+# sns.countplot(ax=axes[1], data=df, x="omo15i2_w2b", hue="type",
+#               order=["Not at all", "1 to 5 times", "6 to 10 times", "11 to 20 times", "21 to 50 times",
+#                      "More than 50 times"])
+# axes[1].set_title("Desired")
+# axes[1].set_xlabel("Frequency")
+# axes[1].set_ylabel("Count")
+# axes[1].set_ylim([0, 2500])
+#
+# fig2, axes = plt.subplots(2, 1)
+# fig2.suptitle("OMO16 - Touch frequency put arm around shoulder last week")
+# sns.countplot(ax=axes[0], data=df, x="omo16i1_w2b", hue="type",
+#               order=["Not at all", "1 to 5 times", "6 to 10 times", "11 to 20 times", "21 to 50 times",
+#                      "More than 50 times"])
+# axes[0].set_title("Received")
+# axes[0].set_xlabel("Frequency")
+# axes[0].set_ylabel("Count")
+# axes[0].set_ylim([0, 2500])
+#
+# sns.countplot(ax=axes[1], data=df, x="omo16i2_w2b", hue="type",
+#               order=["Not at all", "1 to 5 times", "6 to 10 times", "11 to 20 times", "21 to 50 times",
+#                      "More than 50 times"])
+# axes[1].set_title("Desired")
+# axes[1].set_xlabel("Frequency")
+# axes[1].set_ylabel("Count")
+# axes[1].set_ylim([0, 2500])
+#
+# fig3, axes = plt.subplots(2, 1)
+# fig3.suptitle("OMO17 - Touch frequency intimate hug last week")
+# sns.countplot(ax=axes[0], data=df, x="omo17i1_w2b", hue="type",
+#               order=["Not at all", "1 to 5 times", "6 to 10 times", "11 to 20 times", "21 to 50 times",
+#                      "More than 50 times"])
+# axes[0].set_title("Received")
+# axes[0].set_xlabel("Frequency")
+# axes[0].set_ylabel("Count")
+# axes[0].set_ylim([0, 2500])
+#
+# sns.countplot(ax=axes[1], data=df, x="omo17i2_w2b", hue="type",
+#               order=["Not at all", "1 to 5 times", "6 to 10 times", "11 to 20 times", "21 to 50 times",
+#                      "More than 50 times"])
+# axes[1].set_title("Desired")
+# axes[1].set_xlabel("Frequency")
+# axes[1].set_ylabel("Count")
+# axes[1].set_ylim([0, 2500])
+# plt.show()
